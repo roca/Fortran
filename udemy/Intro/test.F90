3253f97b1e069d6c019238a13a90c7d07b53b705
Romel Campbell <romelcampbell@gmail.com>
2026-09-20 21:54:15 -0400

    Latested

--- udemy/Intro/test.F90
+++ udemy/Intro/test.F90
@@ -1,3 +1,11 @@
 program Test
+  implicit none
+  integer(kind=4):: i
+
   print *, "Hello, World!"
+
+  do while (i <= 5)
+    print *, "This is iteration number ", i
+    i = i + 1
+  end do
 end program Test
